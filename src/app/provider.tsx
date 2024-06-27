43cbfb9e34240a24b28ac20744bab907cea3878c
Jens Erik Christiansen <undergroundviking@gmail.com>
2024-06-27 10:10:50 -0700

    section 7.55

---
+++ src/app/provider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { NextUIProvider } from '@nextui-org/react'
+
+type ProvidersProps = {
+    children: React.ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    )
+}
