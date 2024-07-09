5969bf0a554b01f4c694d5c65c82722c95278625
Jens Erik Christiansen <undergroundviking@gmail.com>
2024-07-09 00:40:34 -0700

    section 10.115

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,4 @@ export { default as signOut } from './sign-out';
 export { default as createComment } from './create-comment';
 export { default as createPost } from './create-post';
 export { default as createTopic } from './create-topic';
+export { default as search } from './search';
--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -2,9 +2,14 @@
 
 import { Input } from '@nextui-org/react';
 import { useSearchParams } from 'next/navigation';
+import { search } from '@/actions';
 
 export default function SearchInput() {
   const searchParams = useSearchParams();
 
-  return <Input defaultValue={searchParams.get('term') || ''} />;
+  return (
+    <form action={search}>
+      <Input name="term" defaultValue={searchParams.get('term') || ''} />
+    </form>
+  );
 }
